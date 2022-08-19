acdbb282b311617c4bf96403c8b173a0a9f7e83a
FlakM <maciej.jan.flak@gmail.com>
2022-08-19 07:56:03 +0200

    add flake files hiding aliases for git

--- .nixpkgs/home-manager/modules/git.nix
+++ .nixpkgs/home-manager/modules/git.nix
@@ -19,7 +19,6 @@
     lfs.enable = true;
 
     ignores = [
-      "shell.nix"
       ".metals"
       ".bloop"
       "out"
@@ -32,13 +31,22 @@
       ".bsp/"
       ".scala-build/"
       ".direnv/"
-      "flake.nix"
-      "flake.lock"
+      ".flake_dir/"
     ];
     
     extraConfig = {
       pull = { ff = "only"; };
       init.defaultBranch = "main";
+    };
+
+
+    aliases = {
+      # those two aliases are here to allow rebase when flake files are assumed unchanged
+      # otherwise the rebase would fail even the git status doesn't show anything to be changed
+      # the idea to keep the flake.* added but not commited comes from link:
+      # https://discourse.nixos.org/t/can-i-use-flakes-within-a-git-repo-without-committing-flake-nix/18196/4
+      "flake-hide" = "!git update-index --no-assume-unchanged flake* || true &&  mkdir -p .flake_dir && mv flake* .flake_dir/";
+      "flake-unhide" = "!mv .flake_dir/flake* . || true && git add --intent-to-add flake.* || true && git update-index --assume-unchanged flake* || true";
     };
   };
 }
