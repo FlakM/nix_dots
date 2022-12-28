fba76d8eade2cd4fef11d43789be67c60b6620fd
FlakM <maciej.jan.flak@gmail.com>
2022-12-28 19:40:37 +0100

    ssh

--- .nixpkgs/dell-configuration.nix
+++ .nixpkgs/dell-configuration.nix
@@ -71,6 +71,11 @@
      shell = pkgs.zsh;
      isNormalUser = true;
      extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDh6bzSNqVZ1Ba0Uyp/EqThvDdbaAjsJ4GvYN40f/p9Wl4LcW/MQP8EYLvBTqTluAwRXqFa6fVpa0Y9Hq4kyNG62HiMoQRjujt6d3b+GU/pq7NN8+Oed9rCF6TxhtLdcvJWHTbcq9qIGs2s3eYDlMy+9koTEJ7Jnux0eGxObUaGteQUS1cOZ5k9PQg+WX5ncWa3QvqJNxx446+OzVoHgzZytvXeJMg91gKN9wAhKgfibJ4SpQYDHYcTrOILm7DLVghrcU2aFqLKVTrHSWSugfLkqeorRadHckRDr2VUzm5eXjcs4ESjrG6viKMKmlF1wxHoBrtfKzJ1nR8TGWWeH9NwXJtQ+qRzAhnQaHZyCZ6q4HvPlxxXOmgE+JuU6BCt6YPXAmNEMdMhkqYis4xSzxwWHvko79NnKY72pOIS2GgS6Xon0OxLOJ0mb66yhhZB4hUBb02CpvCMlKSLtvnS+2IcSGeSQBnwBw/wgp1uhr9ieUO/wY5K78w2kYFhR6Iet55gutbikSqDgxzTmuX3Mkjq0L/MVUIRAdmOysrR2Lxlk692IrNYTtUflQLsSfzrp6VQIKPxjfrdFhHIfbPoUdfMf+H06tfwkGONgcej56/fDjFbaHouZ357wcuwDsuMGNRCdyW7QyBXF/Wi28nPq/KSeOdCy+q9KDuOYsX9n/5Rsw== flakm" # content of authorized_keys file
+      # note: ssh-copy-id will add user@clientmachine after the public key
+      # but we can remove the "@clientmachine" part
+    ];
    };
 
 
@@ -190,10 +195,19 @@
     kdeconnect.enable = true;
   };
 
+  services.openssh = {
+    enable = false;
+    # require public key authentication for better security
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    #permitRootLogin = "yes";
+  };
+
+
 
   # Let's open the UDP port with which the network is tunneled through
   networking.firewall = {
-    allowedTCPPorts = [ ];
+    #allowedTCPPorts = [ 22 ];
     allowedUDPPorts = [ 41641 ];
   };
 
