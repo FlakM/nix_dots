f8780574291dc8baeff8fa64b8022ddadae1be01
FlakM <maciej.jan.flak@gmail.com>
2023-09-14 06:59:15 +0200

    chore: add screen lock deamon using swayidle

--- nix_config/home-manager/modules/hyprland.nix
+++ nix_config/home-manager/modules/hyprland.nix
@@ -106,13 +106,27 @@ in
   programs.swaylock = {
     enable = true;
     settings = {
-      color = "808080";
+      color = "000000";
       font-size = 24;
       indicator-idle-visible = false;
       indicator-radius = 100;
       line-color = "ffffff";
       show-failed-attempts = true;
     };
+  };
+
+
+  services.swayidle = {
+    enable = true;
+    events = [
+      { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock";}
+      { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock";}
+      { event = "after-resume"; command = "${pkgs.sway}/bin/swaymsg \"output * toggle\"";}
+    ];
+    timeouts = [
+      { timeout = 600; command = "${pkgs.swaylock}/bin/swaylock";}
+      { timeout = 1200; command = "${pkgs.sway}/bin/swaymsg \"output * toggle\"";}
+    ];
   };
 
   # status bar
