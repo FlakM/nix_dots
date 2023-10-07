f878abc3422d9d7b908c73d013fab7a6ab3c8d86
FlakM <maciej.jan.flak@gmail.com>
2023-10-07 13:27:23 +0200

    chore: setup tls

--- hosts/odroid/nextcloud.nix
+++ hosts/odroid/nextcloud.nix
@@ -17,6 +17,39 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts = {
+      ${config.services.nextcloud.hostName} = {
+        enableACME = false; # Since you're providing your own certs
+        forceSSL = true;
+
+        sslCertificate = "/var/secrets/certs/odroid.tailecbd4.ts.net.crt";
+        sslCertificateKey = "/var/secrets/certs/odroid.tailecbd4.ts.net.key";
+
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:45854"; 
+            proxySetHeaders = {
+              Host = "$host";
+              X-Real-IP = "$remote_addr";
+              X-Forwarded-For = "$proxy_add_x_forwarded_for";
+              X-Forwarded-Proto = "$scheme";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  # You might also want to ensure that the certificates are readable by the nginx process:
+  systemd.tmpfiles.rules = [
+    "a /var/secrets/certs/odroid.tailecbd4.ts.net.crt 0644 root root"
+    "a /var/secrets/certs/odroid.tailecbd4.ts.net.key 0640 root root"
+  ];
 
 }
