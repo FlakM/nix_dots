b2d337465aeb2e7a90f9b7f2187d191e661460b3
FlakM <maciej.jan.flak@gmail.com>
2023-10-07 20:39:31 +0200

    chore: use unstable atuin

--- home-manager/modules/atuin.nix
+++ home-manager/modules/atuin.nix
@@ -2,7 +2,15 @@
 
   programs.atuin = {
     enable = true;
+    package = pkgs.unstable.atuin;
     enableZshIntegration = true;
+    settings = {
+      filter_mode_shell_up_key_binding = "session";
+      auto_sync = true;
+      sync_frequency = "5m";
+      style = "compact";
+      search_mode = "fuzzy";
+    };
   };
 
 }
