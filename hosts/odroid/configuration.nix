d55fe2a605ca70bedb7221addf3bdc3a8afd1255
FlakM <maciej.jan.flak@gmail.com>
2023-10-07 12:40:59 +0200

    chore: add tailscale

--- hosts/odroid/configuration.nix
+++ hosts/odroid/configuration.nix
@@ -14,5 +14,18 @@
 
   environment.systemPackages = with pkgs; [
     home-manager
+    tailscale
   ];
+
+  # Let's open the UDP port with which the network is tunneled through
+  networking.firewall = {
+    allowedUDPPorts = [ 41641 ]; # tailscale
+  };
+
+
+
+  # enable the tailscale daemon; this will do a variety of tasks:
+  # 1. create the TUN network device
+  # 2. setup some IP routes to route through the TUN
+  services.tailscale.enable = true;
 }
