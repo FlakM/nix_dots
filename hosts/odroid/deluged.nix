6adb3ba16594a592f49aec40d246e73992c7e732
FlakM <maciej.jan.flak@gmail.com>
2023-10-21 20:11:35 +0200

    deluged listen ports

--- hosts/odroid/deluged.nix
+++ hosts/odroid/deluged.nix
@@ -53,26 +53,33 @@ let
     
     # Set reverse NAT for replies
     iptables -t nat -A POSTROUTING -o br-nordvpn -p tcp --dport $PORT -d 10.200.2.2 -j SNAT --to-source 10.200.2.1
+
+
+    for PORT in {6881..6891}; do
+        iptables -t nat -A PREROUTING -i $DEFAULT_INTERFACE -p tcp --dport $PORT -j DNAT --to-destination 10.200.2.2:$PORT
+        iptables -A FORWARD -i $DEFAULT_INTERFACE -o br-nordvpn -p tcp --dport $PORT -d 10.200.2.2 -j ACCEPT
+        iptables -t nat -A POSTROUTING -o br-nordvpn -p tcp --dport $PORT -d 10.200.2.2 -j SNAT --to-source 10.200.2.1
+    done
   '';
 in
 {
 
-  systemd.services.nordvpn-setup = {
-    description = "NordVPN Network Setup";
-    path = with pkgs; [ iproute2 iptables bash wireguard-tools wgnord curl openresolv ];
-    script = "${nordvpnSetupScript}";
-    wantedBy = [ "multi-user.target" ];
-    before = [ "deluged.service" "delugeweb.service" ];
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
+  #systemd.services.nordvpn-setup = {
+  #  description = "NordVPN Network Setup";
+  #  path = with pkgs; [ iproute2 iptables bash wireguard-tools wgnord curl openresolv ];
+  #  script = "${nordvpnSetupScript}";
+  #  wantedBy = [ "multi-user.target" ];
+  #  before = [ "deluged.service" "delugeweb.service" ];
+  #  wants = [ "network-online.target" ];
+  #  after = [ "network-online.target" ];
 
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "5s"; # Waits 5 seconds before restarting
-      StartLimitIntervalSec = "60s"; # Check number of restarts within 60 seconds
-      StartLimitBurst = 3; # Allow up to 3 restarts within the interval
-    };
-  };
+  #  serviceConfig = {
+  #    Restart = "on-failure";
+  #    RestartSec = "5s"; # Waits 5 seconds before restarting
+  #    StartLimitIntervalSec = "60s"; # Check number of restarts within 60 seconds
+  #    StartLimitBurst = 3; # Allow up to 3 restarts within the interval
+  #  };
+  #};
 
   services.deluge = {
     enable = true;
@@ -81,7 +88,7 @@ in
       openFirewall = false;
       port = 8112;
     };
-    declarative = true;
+    declarative = false;
     config = {
       download_location = "/var/media/grownups/seriale/";
       #share_ratio_limit = "2.0";
@@ -94,20 +101,25 @@ in
       enable_udp_trackers = true;
       announce_to_all_trackers = true;
       allow_incoming_legacy = true;
+      listen_ports = [ 6881 6891 ];  # This sets the incoming port range
     };
     authFile = "/var/secrets/deluge/auth";
     openFirewall = true;
   };
 
-  systemd.services.deluged.serviceConfig = {
-    NetworkNamespacePath = "/var/run/netns/nordvpn_ns";
-  };
+  
 
-  systemd.services.delugeweb.serviceConfig = {
-    NetworkNamespacePath = "/var/run/netns/nordvpn_ns";
-  };
+  #systemd.services.deluged.serviceConfig = {
+  #  NetworkNamespacePath = "/var/run/netns/nordvpn_ns";
+  #};
+
+  #systemd.services.delugeweb.serviceConfig = {
+  #  NetworkNamespacePath = "/var/run/netns/nordvpn_ns";
+  #};
 
 
+  networking.firewall.allowedTCPPorts = [ 8112 6881 6882 6883 6884 6885 6886 6887 6888 6889 6890 6891 ];
+  networking.firewall.allowedUDPPorts = [ 6881 6882 6883 6884 6885 6886 6887 6888 6889 6890 6891 ];
 
   services.nginx = {
     enable = true;
@@ -122,8 +134,8 @@ in
           extraConfig = ''
             proxy_set_header Host $host; # try $host instead if this doesn't work
             proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_pass http://10.200.2.2:8112; # replace port
-            proxy_redirect http://10.200.2.2:8112 https://delugeweb.house.flakm.com;'';
+            proxy_pass http://127.0.0.1:8112; # replace port
+            proxy_redirect http://127.0.0.1:8112 https://delugeweb.house.flakm.com;'';
         };
       };
     };
