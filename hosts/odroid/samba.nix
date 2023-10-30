b27e3748343be16772db82f4f85f6d1d6044090c
FlakM <maciej.jan.flak@gmail.com>
2023-10-30 12:36:47 +0100

    allow remote connections

--- hosts/odroid/samba.nix
+++ hosts/odroid/samba.nix
@@ -22,8 +22,7 @@
       #use sendfile = yes
       #max protocol = smb2
       # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.0. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
+      hosts allow = 0.0.0.0/0 192.168.0. 127.0.0.1 localhost odroid
       guest account = nobody
       map to guest = bad user
     '';
