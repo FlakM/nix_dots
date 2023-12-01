9115fea4af9870c4a3d55cb2a62d3a2c47f5af83
FlakM <maciej.jan.flak@gmail.com>
2023-12-01 12:04:03 +0100

    chore add yamlfmt

--- shared/k3s.nix
+++ shared/k3s.nix
@@ -7,5 +7,7 @@
   services.k3s.extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
   ];
-  environment.systemPackages = [ pkgs.k3s pkgs.kubernetes-helm pkgs.kubectl ];
+  environment.systemPackages = with pkgs; [ k3s kubernetes-helm kubectl yamlfmt ];
+
+
 }
