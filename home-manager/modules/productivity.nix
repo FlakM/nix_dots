fd3e1d3152d30a67f8fa6d5794132f53228630ea
FlakM <maciej.jan.flak@gmail.com>
2023-12-23 19:59:49 +0100

    chore: add certbot

--- home-manager/modules/productivity.nix
+++ home-manager/modules/productivity.nix
@@ -1,10 +1,12 @@
 { config, pkgs, pkgsUnstable, libs, lib, ... }:
 {
+    nixpkgs.config.permittedInsecurePackages =
+    lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
 
 
   home.packages = with pkgs; [
     timewarrior
-    unstable.obsidian
+    obsidian
   ];
 
   programs.dircolors = {
--- hosts/amd-pc/wildcard_cert.nix
+++ hosts/amd-pc/wildcard_cert.nix
@@ -4,9 +4,15 @@
   #
   # sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/cloudflare.ini -d "*.house.flakm.com" --preferred-challenges dns-01
   #
-  environment.systemPackages = with pkgs; [
-    certbot-full
-  ];
+  #environment.systemPackages = with pkgs; [
+  #  certbot-full
+  #];
+
+  services.certbot = {
+    enable = true;
+    agreeTerms = true;
+    package = pkgs.certbot.withPlugins (ps: with ps; [ python311Packages.certbot-dns-cloudflare ]);
+  };
 
 
 }
