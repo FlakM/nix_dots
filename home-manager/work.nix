20bda150af744338808cfcdeb0188f86fa43cf57
FlakM <maciej.jan.flak@gmail.com>
2024-11-29 22:00:40 +0100

    chore: add work modules

---
+++ home-manager/work.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./modules/home-manager.nix
+    ./modules/common.nix
+
+    # todo uncomment when i fix it
+    ./modules/nvim/neovim.nix
+    ./modules/rust.nix
+    ./modules/tmux.nix
+    ./modules/git.nix
+    ./modules/gpg_home.nix
+
+    ./modules/productivity.nix
+
+    ./modules/yubikey.nix
+
+    ./modules/atuin.nix
+    ./modules/kitty.nix
+    ./modules/starship.nix
+    ./modules/zsh.nix
+
+    ./modules/scala.nix
+  ];
+
+
+  home = {
+    username = "maciek";
+    homeDirectory = "/Users/maciek";
+    stateVersion = "24.05";
+  };
+
+
+  # copied from https://github.com/LnL7/nix-darwin/issues/214#issuecomment-2050027696
+  # to enable trampolines for home-manager
+  home.activation = {
+    rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+      apps_source="$genProfilePath/home-path/Applications"
+      moniker="Home Manager Trampolines"
+      app_target_base="${config.home.homeDirectory}/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target" || true
+    '';
+  };
+
+
+}
---
+++ hosts/work/configuration.nix
@@ -0,0 +1,65 @@
+{ pkgs, inputs, lib, pkgs-unstable, pkgs-master, config, ... }:
+{
+
+
+  system.stateVersion = 5;
+
+  services.nix-daemon.enable = true;
+  services.karabiner-elements.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bat
+    home-manager
+
+
+    rustup
+  ];
+
+
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  environment.shells = with pkgs; [
+    zsh
+    bashInteractive
+  ];
+
+
+  users.users.maciek = {
+    home = "/Users/maciek";
+    shell = pkgs.zsh;
+    uid = 502;
+  };
+
+  users.knownUsers = [
+    "maciek"
+  ];
+
+
+  environment.pathsToLink = [ "/share/zsh" ];
+
+
+
+  services.tailscale.enable = true;
+
+
+  system.activationScripts.postUserActivation.text = ''
+    rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+    apps_source="${config.system.build.applications}/Applications"
+    moniker="Nix Trampolines"
+    app_target_base="$HOME/Applications"
+    app_target="$app_target_base/$moniker"
+    mkdir -p "$app_target"
+    # shellcheck disable=SC2086
+    ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
+  '';
+
+}
+
